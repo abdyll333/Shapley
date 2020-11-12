71e257544e643ff32a36bd137536769905ea27f6
abdyll333 <49811176+abdyll333@users.noreply.github.com>
2020-11-12 14:51:45 +0300

    Math Shapley
    Configured Shapley vector counting

--- compute_shapley.cpp
+++ compute_shapley.cpp
@@ -3,15 +3,20 @@
 #include <QDebug>
 #include <math.h>
 
+
+//Пока на 12.11.2020 Записывать доли от акций можно через setUi_qvPlayerProportionShares() вписав нужные числа возможно будет далее импорт из файла или рандом
+
 compute_shapley::compute_shapley(QObject *parent) : QObject(parent)
 {
-    int n=4; //Временное ограничение по числу игроков
-    iPlayersValue_n=n;
-    iCoalitionMaxVal=pow(2,iPlayersValue_n);
-    qDebug()<<"Максимальное число коалиций = "<<iCoalitionMaxVal<<endl;
-    setLi_qvFactorialCalculatedValues();
     setUi_qvPlayerProportionShares();
+    iPlayersValue_n=ui_qvPlayerProportionShares.size(); //ограничение по числу игроков задается количеством записанных долей
+    iCoalitionMaxVal=pow(2,iPlayersValue_n);
+    qDebug()<<"Максимальное число коалиций = "<<iCoalitionMaxVal<<"\nМаксимальное число долей = "<<iAllSharesVal<<"\nМаксимальное число игроков = "<<iPlayersValue_n<<endl;
+    setLi_qvFactorialCalculatedValues();
     createAllWinStrategyCoalitions();
+    configureVectorShapley();
+    for(int i = 0;i<d_qvectorShapley.size();i++)
+    qDebug()<<"Вектор Шепли ["<<i<<"] ="<<d_qvectorShapley[i];
 
 }
 void compute_shapley::setiPlayersValue_n(int n)
@@ -22,18 +27,30 @@ void compute_shapley::setLi_qvFactorialCalculatedValues()
 {
     for(int i=0;i<=iPlayersValue_n;i++)
     {
-        li_qvFactorialCalculatedValues.push_back(factorial(static_cast<ulong>(i)));
+        d_qvFactorialCalculatedValues.push_back(factorial(static_cast<long double>(i)));
         // qDebug()<<"Factorial["<<i<<"]="<<li_qvFactorialCalculatedValues[i]<<endl;
     }
 
 }
 void compute_shapley::setUi_qvPlayerProportionShares()
 {
-    for(int i=0;i<iPlayersValue_n;i++) //Индексация с 0
-    {
-        ui_qvPlayerProportionShares.push_back(ciAllSharesVal/iPlayersValue_n);
-        //qDebug()<<"Доля участника["<<i<<"]="<<ui_qvPlayerProportionShares[i]<<endl;
-    }
+    iAllSharesVal=0;
+    //Всем поровну (для теста)
+//    for(int i=0;i<iPlayersValue_n;i++) //Индексация с 0
+//    {
+//        ui_qvPlayerProportionShares.push_back(ciAllSharesVal/iPlayersValue_n);
+//        //qDebug()<<"Доля участника["<<i<<"]="<<ui_qvPlayerProportionShares[i]<<endl;
+//    }
+    ui_qvPlayerProportionShares.push_back(35);
+    iAllSharesVal+=35;
+    ui_qvPlayerProportionShares.push_back(25);
+    iAllSharesVal+=25;
+    ui_qvPlayerProportionShares.push_back(22);
+    iAllSharesVal+=22;
+    ui_qvPlayerProportionShares.push_back(18);
+    iAllSharesVal+=18;
+    for(int i =0;i<ui_qvPlayerProportionShares.size();i++)
+        qDebug()<<"Игрок - "<<i<<" Доля игрока = "<<ui_qvPlayerProportionShares[i];
 }
 ulong compute_shapley::factorial(ulong n)
 {
@@ -75,7 +92,7 @@ int compute_shapley::binarray(QVector<int> &arr,int number)
            qvTemparr.push_back(0); //Бинаризация числа (индекс игрока на 1 меньше чем должен быть)
        }
     }
-    if(summ>(ciAllSharesVal/2)) //Условие выйгрыша коалиции
+    if(summ>(iAllSharesVal/2)) //Условие выйгрыша коалиции
     {
         arr=qvTemparr;
         i_qvAllWinCoalitionsIndexSumm.push_back(summ); //Сумма акций всех участников коалиции (индексация по порядку (-1) из всех возможных коалиций)
@@ -84,5 +101,39 @@ int compute_shapley::binarray(QVector<int> &arr,int number)
     }
     return 0;
 }
+double compute_shapley::getLi_qvFactorialCalculatedValues(int i)
+{
+    return d_qvFactorialCalculatedValues[i];
+}
+void compute_shapley::computeShapley_i(int i,int j)
+{
+    int t=i_qvCoalitionValue_t[j]; //находим количество членов j коалиции для формулы
+    int n=iPlayersValue_n; //Число участников
+    double a=getLi_qvFactorialCalculatedValues(t-1);
+    double b=getLi_qvFactorialCalculatedValues(n-t);
+    double c=getLi_qvFactorialCalculatedValues(n);
+    double summ=a*b/c;
+    d_qvectorShapley[i]+=summ; //Суммируем значение для i значения вектора Шепли
+    //qDebug()<<"Итерация: i="<<i<<" j="<<j<<" a="<<a<<" b="<<b<<" c="<<c<<"Значение Шепли = "<<summ;
+}
+void compute_shapley::configureVectorShapley()
+{
+    for(int i=0;i<iPlayersValue_n;i++)
+    d_qvectorShapley.push_back(0);
+    //Сначала выделим решающего члена коалиции, затем вычислим для него вектор Шепли
+    for(int j=0;j<i_qvWinningCoalitions.size();j++) //перебор всех выйгрышных коалицицй
+    {
+        for(int i=0;i<iPlayersValue_n;i++) //Перебор всех участников игры
+        {
+            if(i_qvWinningCoalitions[j][i]==1) //Если такой член коалиции сущесвует, то проверим сможет ли коалиция выйграть без него
+            {
+                int winShare=i_qvAllWinCoalitionsIndexSumm[j]-i_qvWinningCoalitions[j][i]*ui_qvPlayerProportionShares[i];
+                if(winShare<=(iAllSharesVal/2))
+                    computeShapley_i(i,j);
+            }
+        }
+    }
+}
+
 
 
--- compute_shapley.h
+++ compute_shapley.h
@@ -8,19 +8,16 @@
 class compute_shapley : public QObject
 {
     Q_OBJECT
-    const int ciAllSharesVal=100;
+    int iAllSharesVal;
     int iPlayersValue_n; // Число участников n
     int iCoalitionMaxVal; //Число возможных коалиций
 
     QVector<unsigned int>ui_qvPlayerProportionShares ; //Набор участников и их доля акций в компании где i - номер участиника
-
     QVector<QVector<int>>i_qvWinningCoalitions; //Победные коалиции вида {0,1,1..,n}
     QVector<int>i_qvCoalitionValue_t; //Число членов коалиции t для выборки (i соответсвует номеру стратегии)
     QVector<int>i_qvAllWinCoalitionsIndexSumm; //Сумма акций всех победных участников коалиции (индексация по порядку из всех возможных коалиций)
-
-    QVector<long double>ld_qvectorShapley; // вычисляемый вектор Шепли
-
-    QVector<ulong>li_qvFactorialCalculatedValues; //Вычисляемые заранее факториалы для задачи где v[i]-искомое значение i!
+    QVector<double>d_qvFactorialCalculatedValues; //Вычисляемые заранее факториалы для задачи где v[i]-искомое значение i!
+    QVector<double>d_qvectorShapley; // вычисляемый вектор Шепли
 
 public:
     explicit compute_shapley(QObject *parent = nullptr);
@@ -33,6 +30,9 @@ public:
     void createAllWinStrategyCoalitions(); //Составление набора всех неповторяющихся возможных победных стратегий коалиций
     /* Создает из 10сс 2сс масиив (делает выборку для коалиций ) с одновременным посчетом выигрыша (Одновременно создает победную выборку) */
     int binarray(QVector<int> &arr,int number);
+    void computeShapley_i(int i,int j); //Расчитывает по формуле i элемент вектора Шепли из j-той выборки по коалиции i_qvWinningCoalitions и записывает результат в число
+    void configureVectorShapley(); // Собираем все вычисления в вектор Шепли
+    double getLi_qvFactorialCalculatedValues(int i);
 
 
 signals:
